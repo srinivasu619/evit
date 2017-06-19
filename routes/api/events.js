231f325bdf8ee58a3a07d9b6492bf530d8294c1b
srinivasu619 <srinivasu.618@gmail.com>
2017-06-18 22:02:49 -0700

    modified and added event routes

--- routes/api/events.js
+++ routes/api/events.js
@@ -5,13 +5,35 @@ const Event = require('../../db/models').Event;
 
 route.get('/',function(req,res){
 	//res.send("GETTING YOU ALL THE EVENTS");
-	Event.findAll()
+	Event.findAll({
+		// where:{
+		// 	hostId:req.user.id
+		// }
+	})
 	.then(function(events){
 		res.send(events);
 	})
 	.catch(function(err){
-		console.log(err);
+		//console.log(err);
 		res.send("ERROR IN RETRIEVING");
+	})
+});
+
+route.get('/:id',function(req,res){
+	Event.findOne({
+		where:{
+			id: req.params.id,
+		//	hostId:req.user.id
+		}
+	})
+	.then(function(event){
+		if(!event){
+			res.send("THERE IS NO SUCH EVENT");
+		}
+		res.send(event);
+	})
+	.catch(function(err){
+		res.send(err);
 	})
 });
 
@@ -25,13 +47,44 @@ route.post('/new',function(req,res){
 		imgUrl:req.body.imgUrl,
 		startTime:new Date(req.body.startTime),
 		endTime:new Date(req.body.endTime),
-		message:req.body.message
+		message:req.body.message,
+		hostId:req.user.id
 	}).then(function(event){
+		//if(req.body.inv)
 		res.send(event);
 	}).catch(function(err){
-		console.log('err');
+		//console.log('err');
 		res.send('There was an error');
 	})
+});
+
+route.put('/:id',function(req,res){
+	// console.log("\n\n\n\n"+req.body.title);
+	// console.log("\n"+req.body.message);
+	// console.log("\n"+req.body.startTime);
+	// console.log("\n"+req.body.endTime);
+	// console.log("\n"+req.body.venue);
+	Event.update({
+            title: req.body.title,
+            message: req.body.message,
+            startTime: req.body.startTime ? new Date(req.body.startTime) : undefined,
+            endTime: req.body.endTime ? new Date(req.body.endTime) : undefined,
+            imgUrl: req.body.imgUrl,
+            venue: req.body.venue,
+        },
+        {
+            where: {
+                id: req.params.id,
+                hostId: req.user.id
+            }
+        })
+		.then((updatedEvent) => {
+            if (updatedEvent[0] == 0) {
+                return res.status(403).send('Event does not exist, or you cannot edit it')
+            } else {
+                res.status(200).send('Event successfully edited')
+            }
+        })
 });
 
 module.exports = route;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,8 @@
 const router = require("express").Router;
-
+const passport = require('../../auth/passport');
 const route = router();
 
+route.use(passport.authenticate('bearer'));
 route.use('/events',require('./events'));
 route.use('/users',require('./users'));
 
